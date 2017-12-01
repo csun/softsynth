926632bf645ac3d65032c1caa5fc7a2739cb5daa
Cameron Sun <cameron.csun@gmail.com>
2017-11-30 22:52:15 -0500

    Fixed small bugs in delay

--- auto-gen-synth/SoftSynth/Source/DelayProcessor.cpp
+++ auto-gen-synth/SoftSynth/Source/DelayProcessor.cpp
@@ -1,17 +1,14 @@
-#include "DelayProcessor.h"
 #include <cmath>
 #include <iostream>
-
+#include "DelayProcessor.h"
 
 DelayProcessor::DelayProcessor() {
-  addParameter(isOn = new AudioParameterBool(
-        "isOn", "On", false));
-  addParameter(time = new AudioParameterFloat(
-        "time", "Time (ms)", 0.0f, 1000.0f, 200.0f));
-  addParameter(feedback = new AudioParameterFloat(
-        "feedback", "Feedback", 0.0f, 1.0f, 0.25f));
-  addParameter(mix = new AudioParameterFloat(
-        "mix", "Mix", 0.0f, 1.0f, 0.5f));
+  addParameter(isOn = new AudioParameterBool("isOn", "On", false));
+  addParameter(time = new AudioParameterFloat("time", "Time (ms)", 0.0f,
+                                              1000.0f, 200.0f));
+  addParameter(feedback = new AudioParameterFloat("feedback", "Feedback", 0.0f,
+                                                  1.0f, 0.25f));
+  addParameter(mix = new AudioParameterFloat("mix", "Mix", 0.0f, 1.0f, 0.5f));
 }
 
 DelayProcessor::~DelayProcessor() {
@@ -19,23 +16,22 @@ DelayProcessor::~DelayProcessor() {
   // We do not need to free them manually.
 }
 
-void DelayProcessor::prepareToPlay(double sampleRate, int samplesPerBlockExpected) {
+void DelayProcessor::prepareToPlay(double sampleRate,
+                                   int samplesPerBlockExpected) {
   currentSampleRate = sampleRate;
 }
 
-void DelayProcessor::releaseResources() {
-}
+void DelayProcessor::releaseResources() {}
 
 void DelayProcessor::processBlock(AudioSampleBuffer &buffer, MidiBuffer &midi) {
-  if(isOn->get()) {
+  if (isOn->get()) {
     resizeDelayLine();
 
-    float* writeBuf = buffer.getWritePointer(0, 0);
+    float *writeBuf = buffer.getWritePointer(0, 0);
     for (int sample = 0; sample < buffer.getNumSamples(); ++sample) {
       processSample(writeBuf[sample]);
     }
-  }
-  else {
+  } else {
     delayLine.clear();
   }
 }
@@ -47,20 +43,19 @@ void DelayProcessor::processSample(float &sample) {
   float delaySample = delayLine.front();
 
   delayLine.pop_front();
-  delayLine.push_back(
-      (in * (1.0f - feedbackAmt)) + (delaySample * feedbackAmt));
-  
-  sample = (in * (1.0f - mixAmt)) + (delaySample * mixAmt);
+  delayLine.push_back(in + (delaySample * feedbackAmt));
+
+  sample = std::min(
+      1.0f, std::max(0.0f, (in * (1.0f - mixAmt)) + (delaySample * mixAmt)));
 }
 
 void DelayProcessor::resizeDelayLine() {
-  int desiredSize = std::ceil(
-      (time->get() / 1000) * currentSampleRate);
+  int desiredSize = std::ceil((time->get() / 1000) * currentSampleRate);
 
-  while(delayLine.size() < desiredSize) {
+  while (delayLine.size() < desiredSize) {
     delayLine.push_front(0.0f);
   }
-  while(delayLine.size() > desiredSize) {
+  while (delayLine.size() > desiredSize) {
     delayLine.pop_front();
   }
 }
